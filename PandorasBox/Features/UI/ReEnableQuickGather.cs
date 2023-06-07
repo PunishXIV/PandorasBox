265dc26044f09ac66404e572dbf9ecce87bfa8be
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2023-06-07 18:39:36 +1000

    added proper node check and collectable check

--- PandorasBox/Features/UI/ReEnableQuickGather.cs
+++ PandorasBox/Features/UI/ReEnableQuickGather.cs
@@ -1,5 +1,9 @@
+using Dalamud.Game.ClientState.Conditions;
 using ECommons.Automation;
 using ECommons.DalamudServices;
+using ECommons.Logging;
+using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using Lumina.Excel.GeneratedSheets;
 using PandorasBox.FeaturesSetup;
@@ -7,6 +11,8 @@ using PandorasBox.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
+using static ECommons.GenericHelpers;
 
 namespace PandorasBox.Features.UI
 {
@@ -18,66 +24,95 @@ namespace PandorasBox.Features.UI
 
         public override FeatureType FeatureType => FeatureType.UI;
 
-        public override void Enable()
+        public enum GatheringPointType : byte
         {
-            Svc.Framework.Update += RunFeature;
-            base.Enable();
+            None,
+            Basic,
+            Unspoiled,
+            Leve,
+            Ephemeral,  // for aetherial reduction,
+            Folklore,
+            SFShadow,   // spearfishing special
+            DiademBasic,
+            DiademClouded,  // diadem special
         }
 
         private void RunFeature(Dalamud.Game.Framework framework)
         {
-            if (Svc.GameGui.GetAddonByName("Gathering") != IntPtr.Zero)
+            try
             {
-                var ptr = (AtkUnitBase*)Svc.GameGui.GetAddonByName("Gathering");
-                if (ptr == null) return;
-
-                var nearbyNodes = Svc.Objects.Where(x => (x.ObjectKind == Dalamud.Game.ClientState.Objects.Enums.ObjectKind.GatheringPoint || x.ObjectKind == Dalamud.Game.ClientState.Objects.Enums.ObjectKind.CardStand) && GameObjectHelper.GetTargetDistance(x) < 2 && GameObjectHelper.GetHeightDifference(x) <= 3).ToList();
-
-                var nearestNode = nearbyNodes.OrderBy(GameObjectHelper.GetTargetDistance).First();
-
-                // only function on Unspoiled nodes
-                if (!(Svc.Data.GetExcelSheet<GatheringPointTransient>().Any(x => x.RowId == nearestNode.DataId && x.GatheringRarePopTimeTable.Value.RowId > 0))) return;
-
-                var checkboxNode = ptr->UldManager.NodeList[10]; // quick gather checkbox
-                checkboxNode->ToggleVisibility(true);
-                try
+                if (TryGetAddonByName<AddonGathering>("Gathering", out var addon))
                 {
+
+                    var ptr = (AtkUnitBase*)addon;
+                    if (ptr == null) return;
+
+                    // var nearbyNodes = Svc.Objects.Where(x => (x.ObjectKind == Dalamud.Game.ClientState.Objects.Enums.ObjectKind.GatheringPoint || x.ObjectKind == Dalamud.Game.ClientState.Objects.Enums.ObjectKind.CardStand) && GameObjectHelper.GetTargetDistance(x) < 2 && GameObjectHelper.GetHeightDifference(x) <= 3).ToList();
+
+                    // var nearestNode = nearbyNodes.OrderBy(GameObjectHelper.GetTargetDistance).First();
+
+                    // only function on Unspoiled nodes
+                    // if (!(Svc.Data.GetExcelSheet<GatheringPointTransient>().Any(x => x.RowId == nearestNode.DataId && x.GatheringRarePopTimeTable.Value.RowId > 0))) return;
+
+                    // return if it's a normal, diadem, or fishing node
+                    var targetNode = Svc.Targets.Target;
+                    if (!(Svc.Data.GetExcelSheet<GatheringPoint>().Any(x => x.RowId == targetNode.DataId && x.Type is 2 or 3 or 4 or 5))) return;
+
+                    var checkboxNode = ptr->UldManager.NodeList[10];
+                    checkboxNode->ToggleVisibility(true);
                     bool isChecked = checkboxNode->GetAsAtkComponentCheckBox()->IsChecked;
                     if (!isChecked) return;
 
-                    var items = new List<IntPtr>
+                    var items = new List<bool>
                     {
-                      (IntPtr)ptr->UldManager.NodeList[25]->GetAsAtkComponentNode()->Component->UldManager.NodeList[1],
-                      (IntPtr)ptr->UldManager.NodeList[24]->GetAsAtkComponentNode()->Component->UldManager.NodeList[1],
-                      (IntPtr)ptr->UldManager.NodeList[23]->GetAsAtkComponentNode()->Component->UldManager.NodeList[1],
-                      (IntPtr)ptr->UldManager.NodeList[22]->GetAsAtkComponentNode()->Component->UldManager.NodeList[1],
-                      (IntPtr)ptr->UldManager.NodeList[21]->GetAsAtkComponentNode()->Component->UldManager.NodeList[1],
-                      (IntPtr)ptr->UldManager.NodeList[20]->GetAsAtkComponentNode()->Component->UldManager.NodeList[1],
-                      (IntPtr)ptr->UldManager.NodeList[19]->GetAsAtkComponentNode()->Component->UldManager.NodeList[1],
-                      (IntPtr)ptr->UldManager.NodeList[18]->GetAsAtkComponentNode()->Component->UldManager.NodeList[1]
+                      ptr->UldManager.NodeList[25]->GetAsAtkComponentCheckBox()->IsChecked,
+                      ptr->UldManager.NodeList[24]->GetAsAtkComponentCheckBox()->IsChecked,
+                      ptr->UldManager.NodeList[23]->GetAsAtkComponentCheckBox()->IsChecked,
+                      ptr->UldManager.NodeList[22]->GetAsAtkComponentCheckBox()->IsChecked,
+                      ptr->UldManager.NodeList[21]->GetAsAtkComponentCheckBox()->IsChecked,
+                      ptr->UldManager.NodeList[20]->GetAsAtkComponentCheckBox()->IsChecked,
+                      ptr->UldManager.NodeList[19]->GetAsAtkComponentCheckBox()->IsChecked,
+                      ptr->UldManager.NodeList[18]->GetAsAtkComponentCheckBox()->IsChecked
                     };
 
-                    int gatheredNode = -1;
+                    // var nodeIntegrity = 4;
+                    // var locationEffectNode = ptr->UldManager.NodeList[8];
+                    // if (locationEffectNode->IsVisible) nodeIntegrity = 4 + Regex.Match(locationEffectNode->GetAsAtkTextNode()->NodeText.ToString(), @"Gathering Attempts/Integrity \+(\d+)").Groups[1].Value;
+
+                    var gatherablesIds = new List<uint> {
+                        addon->GatheredItemId1, addon->GatheredItemId2, addon->GatheredItemId3, addon->GatheredItemId4, addon->GatheredItemId5, addon->GatheredItemId6, addon->GatheredItemId7, addon->GatheredItemId8
+                    };
+                    int gatheredItemIndex = -1;
                     for (var i = 0; i < items.Count; i++)
                     {
-                        AtkResNode* item = (AtkResNode*)items[i];
-                        if (item->IsVisible)
+                        if (items[i])
                         {
-                            gatheredNode = i;
+                            gatheredItemIndex = i;
                         }
                     }
-                    if (gatheredNode == -1) return;
+
+                    // var gatheredItem = Svc.Data.GetExcelSheet<Item>().Where(x => x.RowId == gatherablesIds[gatheredItemIndex]).First();
+                    // ideally, if it's a collectable, uncheck the box as a form of in-game feedback that it's invalid
+                    if (gatheredItemIndex == -1) return;
+                    // unchecking mid operation does nothing?
                     if (isChecked)
                     {
                         TaskManager.DelayNext("GatheringDelay", 100);
-                        TaskManager.Enqueue(() => Callback.Fire(ptr, false, gatheredNode));
+                        TaskManager.Enqueue(() => Svc.Condition[ConditionFlag.Gathering] && !Svc.Condition[ConditionFlag.Gathering42]);
+                        TaskManager.Enqueue(() => Callback.Fire(ptr, false, gatheredItemIndex));
                     }
                 }
-                catch
-                {
-                    return;
-                }
             }
+            catch
+            {
+                return;
+            }
+        }
+
+        public override void Enable()
+        {
+            Svc.Framework.Update += RunFeature;
+            base.Enable();
         }
 
         public override void Disable()
