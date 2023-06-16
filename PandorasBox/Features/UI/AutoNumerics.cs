50ee9f12fb0a78525ab14a62b074284ed13ef11e
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2023-06-16 15:35:06 +1000

    trade handling

--- PandorasBox/Features/UI/AutoNumerics.cs
+++ PandorasBox/Features/UI/AutoNumerics.cs
@@ -1,13 +1,10 @@
-using ClickLib.Clicks;
+using Dalamud.Game.ClientState.Conditions;
+using Dalamud.Interface.Components;
 using ECommons.Automation;
 using ECommons.DalamudServices;
-using ECommons.Logging;
-using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using ImGuiNET;
 using PandorasBox.FeaturesSetup;
-using System;
-using static ECommons.GenericHelpers;
 
 namespace PandorasBox.Features.UI
 {
@@ -24,6 +21,7 @@ namespace PandorasBox.Features.UI
         public class Configs : FeatureConfig
         {
             public int MinOrMax = 1;
+            public bool ManualTradesOnly = false;
         }
 
         public override void Enable()
@@ -33,16 +31,43 @@ namespace PandorasBox.Features.UI
             base.Enable();
         }
 
+
         private void RunFeature(Dalamud.Game.Framework framework)
         {
-            var numeric = (AtkUnitBase*)Svc.GameGui.GetAddonByName("InputNumeric", 1);
+            var numeric = (AtkUnitBase*)Svc.GameGui.GetAddonByName("InputNumeric");
             if (numeric == null) return;
-            if (numeric->IsVisible && TryGetAddonByName<AddonLookingForGroupDetail>("InputNumeric", out var addon))
+            if (numeric->IsVisible)
             {
-                if (Config.MinOrMax == 0)
-                    TaskManager.Enqueue(() => Callback.Fire(&addon->AtkUnitBase, true, numeric->AtkValues[2].Int));
-                else
-                    TaskManager.Enqueue(() => Callback.Fire(&addon->AtkUnitBase, true, numeric->AtkValues[3].Int));
+                try
+                {
+                    if (Config.MinOrMax != -1 && Config.ManualTradesOnly && Svc.Condition[ConditionFlag.TradeOpen])
+                    {
+                        var tradeAmt = numeric->UldManager.NodeList[4]->GetAsAtkComponentNode()->Component->UldManager.NodeList[4]->GetAsAtkTextNode()->NodeText.ToString();
+                        int num;
+
+                        if (int.TryParse(tradeAmt, out num) && num > 0 && !numeric->UldManager.NodeList[4]->GetAsAtkComponentNode()->Component->UldManager.NodeList[6]->IsVisible)
+                            TaskManager.Enqueue(() => Callback.Fire(numeric, true, int.Parse(tradeAmt)));
+                    }
+                    else
+                    {
+                        if (Config.MinOrMax == 0)
+                            TaskManager.Enqueue(() => Callback.Fire(numeric, true, numeric->AtkValues[2].Int));
+                        if (Config.MinOrMax == 1)
+                            TaskManager.Enqueue(() => Callback.Fire(numeric, true, numeric->AtkValues[3].Int));
+                        if (Config.MinOrMax == -1)
+                        {
+                            var tradeAmt = numeric->UldManager.NodeList[4]->GetAsAtkComponentNode()->Component->UldManager.NodeList[4]->GetAsAtkTextNode()->NodeText.ToString();
+                            int num;
+
+                            if (int.TryParse(tradeAmt, out num) && num > 0 && !numeric->UldManager.NodeList[4]->GetAsAtkComponentNode()->Component->UldManager.NodeList[6]->IsVisible)
+                                TaskManager.Enqueue(() => Callback.Fire(numeric, true, int.Parse(tradeAmt)));
+                        }
+                    }
+                }
+                catch
+                {
+                    return;
+                }
             }
             else
             {
@@ -60,14 +85,20 @@ namespace PandorasBox.Features.UI
 
         protected override DrawConfigDelegate DrawConfigTree => (ref bool _) =>
         {
-            if (ImGui.RadioButton($"Auto set at highest amount possible", Config.MinOrMax == 1))
+            if (ImGui.RadioButton($"Auto confirm highest amount possible", Config.MinOrMax == 1))
             {
                 Config.MinOrMax = 1;
             }
-            if (ImGui.RadioButton($"Auto set at lowest amount possible", Config.MinOrMax == 0))
+            if (ImGui.RadioButton($"Auto confirm lowest amount possible", Config.MinOrMax == 0))
             {
                 Config.MinOrMax = 0;
             }
+            if (ImGui.RadioButton($"Auto confirm manually entered amounts", Config.MinOrMax == -1))
+            {
+                Config.MinOrMax = -1;
+            }
+            ImGui.Checkbox("\"Auto confirm manually\" for trading only", ref Config.ManualTradesOnly);
+            ImGuiComponents.HelpMarker("Will only do \"Auto confirm manually entered amounts\" for trading.\nDefaults to set highest/lowest behaviour for anything else.");
         };
     }
 }
