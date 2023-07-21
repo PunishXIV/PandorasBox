e4d38ee81493707989433a0302e0a8b1b22db865
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2023-07-22 01:00:49 +1000

    setting

--- PandorasBox/Features/Commands/QuickPlateLink.cs
+++ PandorasBox/Features/Commands/QuickPlateLink.cs
@@ -19,31 +19,46 @@ namespace PandorasBox.Features.Commands
         public override string Command { get; set; } = "/pglamlink";
         public override string[] Alias => new string[] { "/pgl" };
 
-        public override List<string> Parameters => new() { "[jobs/roles/gearsets]", "[num]" };
+        public override List<string> Parameters => new() { "jobs", "num" };
         public override string Description => "For quickly linking multiple gearsets to a glamour plate.";
 
         public override FeatureType FeatureType => FeatureType.Commands;
+
+        public struct Gearset
+        {
+            public byte ID { get; set; }
+            public int Slot { get; set; }
+            public string Name { get; set; }
+            public uint ClassJob { get; set; }
+            public byte GlamPlate { get; set; }
+        }
+
+        private List<Gearset> gearsets = new();
+        private List<string> roles = new() { "tank", "healer", "dps", "caster", "melee", "physical ranged", "ranged", "magical ranged" };
         protected override void OnCommand(List<string> args)
         {
-            if (!int.TryParse(args[^1], out var noInts))
-            {
-                PrintModuleMessage("Invalid glamour plate number");
-                return;
-            }
+            gearsets.Clear();
 
             if (args.Count < 2)
             {
                 PrintModuleMessage("Invalid number of arguments");
                 return;
             }
-
-            foreach (var p in Parameters)
+            if (!int.TryParse(args[^1], out var noInts))
             {
-                var jobNames = Svc.Data.GetExcelSheet<ClassJob>().First(x => x.Name.RawString.ToLower() == p.ToLower());
-                var jobAbbrs = Svc.Data.GetExcelSheet<ClassJob>().First(x => x.Abbreviation.RawString.ToLower() == p.ToLower());
+                PrintModuleMessage("Invalid glamour plate number");
+                return;
+            }
 
-                var gearsetModule = RaptureGearsetModule.Instance();
-                var isGearsetName = false;
+            var gearsetModule = RaptureGearsetModule.Instance();
+            foreach (var arg in args)
+            {
+                //if (IsRoleMatch(arg, out var role))
+                //    switch (role)
+                //    {
+                //        case "tank":
+                //            break;
+                //    }
                 for (var i = 0; i < 100; i++)
                 {
                     var gs = gearsetModule->Gearset[i];
@@ -51,17 +66,38 @@ namespace PandorasBox.Features.Commands
                         continue;
 
                     var name = MemoryHelper.ReadString(new IntPtr(gs->Name), 47);
-                    isGearsetName = p.ToLower() == name.ToLower() ? true : false;
-                }
 
-                if (jobNames != null || jobAbbrs != null || isGearsetName)
-                    return;
-
-                if (args.Any(x => x == p))
-                {
-                    Svc.Chat.Print($"Test command executed with argument {p}.");
+                    if (arg.Equals(name, StringComparison.CurrentCultureIgnoreCase) || arg.Equals(gs->ID.ToString(), StringComparison.CurrentCultureIgnoreCase))
+                        gearsets.Add(new Gearset
+                        {
+                            ID = gs->ID,
+                            Slot = i + 1,
+                            ClassJob = gs->ClassJob,
+                            Name = name,
+                            GlamPlate = byte.Parse(args[^1]),
+                        });
                 }
             }
+
+            foreach (var gs in gearsets)
+            {
+                gearsetModule->LinkGlamourPlate(gs.ID, gs.GlamPlate);
+                PrintModuleMessage($"Changed gearset {gs.Name} to use plate {gs.GlamPlate}");
+            }
+        }
+        private bool IsRoleMatch(string input, out string matchedRole)
+        {
+            // Split the input string into individual words
+            var inputSplit = input.ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            // Find roles that partially match the input words
+            matchedRole = roles.FirstOrDefault(role =>
+            {
+                var rolesSplit = role.ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                return inputSplit.All(x => rolesSplit.Any(roleWord => roleWord.Contains(x)));
+            });
+
+            return matchedRole != null;
         }
     }
 }
