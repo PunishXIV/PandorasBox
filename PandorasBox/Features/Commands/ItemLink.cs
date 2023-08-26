ac9ee8e454fde1d56e62a0fb3997f39995a0fb85
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2023-08-26 11:56:24 +1000

    Create ItemLink.cs

---
+++ PandorasBox/Features/Commands/ItemLink.cs
@@ -0,0 +1,79 @@
+using Dalamud.Game.Text.SeStringHandling;
+using Dalamud.Game.Text;
+using ECommons.DalamudServices;
+using PandorasBox.FeaturesSetup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Lumina.Excel.GeneratedSheets;
+using Dalamud.Game.Text.SeStringHandling.Payloads;
+
+namespace PandorasBox.Features.Commands
+{
+    public unsafe class ItemLinkCommand : CommandFeature
+    {
+        public override string Name => "Item Link";
+        public override string Command { get; set; } = "/plink";
+        public override string[] Alias => new string[] { "" };
+
+        public override List<string> Parameters => new() { "<item name || id>" };
+        public override string Description => "It's like the other item link commands, but allows searching.";
+
+        public override FeatureType FeatureType => FeatureType.Commands;
+
+        protected override void OnCommand(List<string> args)
+        {
+
+            var item = Svc.Data.GetExcelSheet<Item>(Svc.ClientState.ClientLanguage).GetRow(0);
+            var argName = string.Join(' ', args);
+
+            //var processName = string.Join(' ', args.Select(arg => arg.Replace("\"", "")).ToList());
+            //PrintModuleMessage($"reg name: {argName}, pro name: {processName}");
+
+            if (uint.TryParse(args[0].Trim(), out var id))
+            {
+                //PrintModuleMessage("entered uint parse");
+                item = Svc.Data.GetExcelSheet<Item>(Svc.ClientState.ClientLanguage).GetRow(id);
+            }
+            else
+            {
+                //PrintModuleMessage("entered name match");
+                item = Svc.Data.GetExcelSheet<Item>().FirstOrDefault(x => x.Name.RawString.Contains(argName, StringComparison.CurrentCultureIgnoreCase));
+            }
+
+            if (id == 0)
+            {
+                PrintModuleMessage($"Item {argName} not found");
+                return;
+            }
+
+            var idStr = new SeStringBuilder()
+                .AddUiForeground(id.ToString(), 1)
+                .Build();
+
+            var sb = new SeStringBuilder()
+                .AddUiForeground("\uE078 ", 32)
+                .Append($"Item #{idStr}: {GetItemLink(id)}");
+
+            Svc.Chat.PrintChat(new XivChatEntry
+            {
+                Message = sb.Build(),
+                Type = XivChatType.Echo
+            });
+        }
+
+        public static SeString GetItemLink(uint id)
+        {
+            var item = Svc.Data.GetExcelSheet<Item>(Svc.ClientState.ClientLanguage).GetRow(id);
+            if (item == null)
+                return new SeString(new TextPayload($"Item#{id}"));
+
+
+            var link = SeString.CreateItemLink(item, false);
+            // TODO: remove in Dalamud v9
+            link.Payloads.Add(UIGlowPayload.UIGlowOff);
+            link.Payloads.Add(UIForegroundPayload.UIForegroundOff);
+            return link;
+        }
+    }
+}
