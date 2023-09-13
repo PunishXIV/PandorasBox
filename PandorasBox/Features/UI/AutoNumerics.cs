0c9bdee124d1f6d178e0fbca8333fc8409f8a75a
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2023-09-13 16:01:52 +0100

    performance improvements

--- PandorasBox/Features/UI/AutoNumerics.cs
+++ PandorasBox/Features/UI/AutoNumerics.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Linq;
 using System.Text;
 using Dalamud.Game;
 using Dalamud.Game.ClientState.Conditions;
 using Dalamud.Logging;
+using ECommons;
 using ECommons.Automation;
 using ECommons.DalamudServices;
 using FFXIVClientStructs.FFXIV.Component.GUI;
@@ -67,55 +69,42 @@ namespace PandorasBox.Features.UI
         public override void Enable()
         {
             Config = LoadConfig<Configs>() ?? new Configs();
-            Svc.Framework.Update += FillRegularNumeric;
-            Svc.Framework.Update += FillVentureNumeric;
-            //Svc.Framework.Update += FillBankNumeric;
+            Common.OnAddonSetup += FillRegularNumeric;
+            Common.OnAddonSetup += FillVentureNumeric;
+            //Common.OnAddonSetup += FillBankNumeric;
             base.Enable();
         }
 
-        private void FillRegularNumeric(Framework framework)
+        private void FillRegularNumeric(SetupAddonArgs obj)
         {
-            var numeric = (AtkUnitBase*)Svc.GameGui.GetAddonByName("InputNumeric");
-            if (numeric == null) { hasDisabled = false; return; }
+            if (obj.AddonName != "InputNumeric") return;
+            if (ImGui.GetIO().KeyShift) return;
 
-            if (numeric->IsVisible && ECommons.GenericHelpers.IsAddonReady(numeric) && ImGui.GetIO().KeyShift) { hasDisabled = true; return; }
-
-            if (numeric->IsVisible && ECommons.GenericHelpers.IsAddonReady(numeric) && !hasDisabled)
+            try
             {
-                try
-                {
-                    var minValue = numeric->AtkValues[2].Int;
-                    var maxValue = numeric->AtkValues[3].Int;
-                    var numericTextNode = numeric->UldManager.NodeList[4]->GetAsAtkComponentNode()->Component->UldManager.NodeList[4]->GetAsAtkTextNode();
-                    var numericResNode = numeric->UldManager.NodeList[4]->GetAsAtkComponentNode()->Component->UldManager.NodeList[6];
+                var minValue = obj.Addon->AtkValues[2].Int;
+                var maxValue = obj.Addon->AtkValues[3].Int;
+                var numericTextNode = obj.Addon->UldManager.NodeList[4]->GetAsAtkComponentNode()->Component->UldManager.NodeList[4]->GetAsAtkTextNode();
+                var numericResNode = obj.Addon->UldManager.NodeList[4]->GetAsAtkComponentNode()->Component->UldManager.NodeList[6];
 
-                    if (Config.WorkOnTrading && Svc.Condition[ConditionFlag.TradeOpen])
-                        TryFill(numeric, minValue, maxValue, Config.TradeMinOrMax, Config.TradeExcludeSplit, Config.TradeConfirm);
+                if (Config.WorkOnTrading && Svc.Condition[ConditionFlag.TradeOpen])
+                    TryFill(obj.Addon, minValue, maxValue, Config.TradeMinOrMax, Config.TradeExcludeSplit, Config.TradeConfirm);
 
-                    else if (Config.WorkOnFCChest && InFcChest())
-                        TryFill(numeric, minValue, maxValue, Config.FCChestMinOrMax, Config.FCExcludeSplit, Config.FCChestConfirm);
+                if (Config.WorkOnFCChest && InFcChest())
+                    TryFill(obj.Addon, minValue, maxValue, Config.FCChestMinOrMax, Config.FCExcludeSplit, Config.FCChestConfirm);
 
-                    else if (Config.WorkOnRetainers && Svc.Condition[ConditionFlag.OccupiedSummoningBell] && !InFcChest())
-                        TryFill(numeric, minValue, maxValue, Config.RetainersMinOrMax, Config.RetainerExcludeSplit, Config.RetainersConfirm);
+                if (Config.WorkOnRetainers && Svc.Condition[ConditionFlag.OccupiedSummoningBell] && !InFcChest())
+                    TryFill(obj.Addon, minValue, maxValue, Config.RetainersMinOrMax, Config.RetainerExcludeSplit, Config.RetainersConfirm);
 
-                    else if (Config.WorkOnMail && InMail())
-                        TryFill(numeric, minValue, maxValue, Config.MailMinOrMax, Config.MailExcludeSplit, Config.MailConfirm);
+                if (Config.WorkOnMail && InMail())
+                    TryFill(obj.Addon, minValue, maxValue, Config.MailMinOrMax, Config.MailExcludeSplit, Config.MailConfirm);
 
-                    else if (Config.WorkOnTransmute && InTransmute())
-                        TryFill(numeric, minValue, maxValue, Config.TransmuteMinOrMax, Config.TransmuteExcludeSplit, Config.TransmuteConfirm);
-
-                    else
-                        return;
-                }
-                catch
-                {
-                    return;
-                }
+                if (Config.WorkOnTransmute && InTransmute())
+                    TryFill(obj.Addon, minValue, maxValue, Config.TransmuteMinOrMax, Config.TransmuteExcludeSplit, Config.TransmuteConfirm);
             }
-            else
+            catch (Exception ex)
             {
-                TaskManager.Abort();
-                return;
+                ex.Log();
             }
         }
 
@@ -145,96 +134,77 @@ namespace PandorasBox.Features.UI
             }
         }
 
-        private void FillBankNumeric(Framework framework)
+        private void FillBankNumeric(SetupAddonArgs obj)
         {
-            var bankNumeric = (AtkUnitBase*)Svc.GameGui.GetAddonByName("Bank");
-            if (bankNumeric == null) { hasDisabled = false; return; }
+            if (obj.AddonName != "Bank") return;
+            if (ImGui.GetIO().KeyShift) return;
+            if (!Config.WorkOnFCChest) return;
 
-            if (bankNumeric->IsVisible && ECommons.GenericHelpers.IsAddonReady(bankNumeric) && ImGui.GetIO().KeyShift) { hasDisabled = true; return; }
-
-            if (Config.WorkOnFCChest && bankNumeric->IsVisible && ECommons.GenericHelpers.IsAddonReady(bankNumeric) && !hasDisabled)
+            try
             {
-                try
-                {
-                    var bMinValue = bankNumeric->AtkValues[5].Int;
-                    var bMaxValue = bankNumeric->AtkValues[6].Int;
-                    var bNumericTextNode = bankNumeric->UldManager.NodeList[4]->GetAsAtkComponentNode()->Component->UldManager.NodeList[4]->GetAsAtkTextNode();
+                var bMinValue = obj.Addon->AtkValues[5].Int;
+                var bMaxValue = obj.Addon->AtkValues[6].Int;
+                var bNumericTextNode = obj.Addon->UldManager.NodeList[4]->GetAsAtkComponentNode()->Component->UldManager.NodeList[4]->GetAsAtkTextNode();
 
-                    if (Config.FCExcludeSplit && IsSplitAddon()) { return; }
-                    if (Config.FCChestMinOrMax == 0)
-                    {
-                        TaskManager.Enqueue(() => bNumericTextNode->SetText(ConvertToByte(bMinValue)));
-                        if (Config.FCChestConfirm)
-                            TaskManager.Enqueue(() => Callback.Fire(bankNumeric, true, 3, (uint)bMinValue));
-                    }
-                    if (Config.FCChestMinOrMax == 1)
-                    {
-                        TaskManager.Enqueue(() => bNumericTextNode->SetText(ConvertToByte(bMaxValue)));
-                        if (Config.FCChestConfirm)
-                            TaskManager.Enqueue(() => Callback.Fire(bankNumeric, true, 3, (uint)bMaxValue));
-                    }
-                    if (Config.FCChestMinOrMax == -1)
-                    {
-                        var currentAmt = bNumericTextNode->NodeText.ToString();
-                        if (int.TryParse(currentAmt, out var num) && num > 0 && bankNumeric->AtkValues[4].Int > 0)
-                            TaskManager.Enqueue(() => Callback.Fire(bankNumeric, true, 0));
-                    }
-                }
-                catch
+                if (Config.FCExcludeSplit && IsSplitAddon()) { return; }
+                if (Config.FCChestMinOrMax == 0)
                 {
-                    return;
+                    TaskManager.Enqueue(() => bNumericTextNode->SetText(ConvertToByte(bMinValue)));
+                    if (Config.FCChestConfirm)
+                        TaskManager.Enqueue(() => Callback.Fire(obj.Addon, true, 3, (uint)bMinValue));
+                }
+                if (Config.FCChestMinOrMax == 1)
+                {
+                    TaskManager.Enqueue(() => bNumericTextNode->SetText(ConvertToByte(bMaxValue)));
+                    if (Config.FCChestConfirm)
+                        TaskManager.Enqueue(() => Callback.Fire(obj.Addon, true, 3, (uint)bMaxValue));
+                }
+                if (Config.FCChestMinOrMax == -1)
+                {
+                    var currentAmt = bNumericTextNode->NodeText.ToString();
+                    if (int.TryParse(currentAmt, out var num) && num > 0 && obj.Addon->AtkValues[4].Int > 0)
+                        TaskManager.Enqueue(() => Callback.Fire(obj.Addon, true, 0));
                 }
             }
-            else
+            catch
             {
-                TaskManager.Abort();
                 return;
             }
         }
 
-        private void FillVentureNumeric(Framework framework)
+        private void FillVentureNumeric(SetupAddonArgs obj)
         {
-            var ventureNumeric = (AtkUnitBase*)Svc.GameGui.GetAddonByName("ShopExchangeCurrencyDialog");
-            if (ventureNumeric == null) { hasDisabled = false; return; }
+            if (obj.AddonName != "ShopExchangeCurrencyDialog") return;
+            if (ImGui.GetIO().KeyShift) return;
+            if (!Config.WorkOnVentures) return;
 
-            if (ventureNumeric->IsVisible && ECommons.GenericHelpers.IsAddonReady(ventureNumeric) && ImGui.GetIO().KeyShift) { hasDisabled = true; return; }
-
-            if (Config.WorkOnVentures && ventureNumeric->IsVisible && ECommons.GenericHelpers.IsAddonReady(ventureNumeric) && !hasDisabled)
+            try
             {
-                try
+                var minValue = 1;
+                var maxAvailable = obj.Addon->AtkValues[5].UInt - obj.Addon->AtkValues[4].UInt;
+                var maxAfford = obj.Addon->AtkValues[1].UInt / obj.Addon->AtkValues[2].UInt;
+                var maxValue = maxAvailable > maxAfford ? maxAfford : maxAvailable;
+
+                var numericTextNode = obj.Addon->UldManager.NodeList[8]->GetAsAtkComponentNode()->Component->UldManager.NodeList[4]->GetAsAtkTextNode();
+
+                if (Config.VentureMinOrMax == 0)
                 {
-                    var minValue = 1;
-                    var maxAvailable = ventureNumeric->AtkValues[5].UInt - ventureNumeric->AtkValues[4].UInt;
-                    var maxAfford = ventureNumeric->AtkValues[1].UInt / ventureNumeric->AtkValues[2].UInt;
-                    var maxValue = maxAvailable > maxAfford ? maxAfford : maxAvailable;
-
-                    var numericTextNode = ventureNumeric->UldManager.NodeList[8]->GetAsAtkComponentNode()->Component->UldManager.NodeList[4]->GetAsAtkTextNode();
-
-                    if (Config.VentureExcludeSplit && IsSplitAddon()) return;
-                    if (Config.VentureMinOrMax == 0)
-                    {
-                        TaskManager.Enqueue(() => numericTextNode->SetText(ConvertToByte(minValue)));
-                        if (Config.VentureConfirm)
-                            TaskManager.Enqueue(() => Callback.Fire(ventureNumeric, true, 0, minValue));
-                    }
-                    if (Config.VentureMinOrMax == 1)
-                    {
-                        TaskManager.Enqueue(() => numericTextNode->SetText(ConvertToByte((int)maxValue)));
-                        if (Config.VentureConfirm)
-                            TaskManager.Enqueue(() => Callback.Fire(ventureNumeric, true, 0, maxValue));
-                    }
-
-                    // No way to detect manually entered amounts
+                    TaskManager.Enqueue(() => numericTextNode->SetText(ConvertToByte(minValue)));
+                    if (Config.VentureConfirm)
+                        TaskManager.Enqueue(() => Callback.Fire(obj.Addon, true, 0, minValue));
                 }
-                catch
+                if (Config.VentureMinOrMax == 1)
                 {
-                    return;
+                    TaskManager.Enqueue(() => numericTextNode->SetText(ConvertToByte((int)maxValue)));
+                    if (Config.VentureConfirm)
+                        TaskManager.Enqueue(() => Callback.Fire(obj.Addon, true, 0, maxValue));
                 }
+
+                // No way to detect manually entered amounts
             }
-            else
+            catch (Exception ex)
             {
-                TaskManager.Abort();
-                return;
+                ex.Log();
             }
         }
 
@@ -280,9 +250,9 @@ namespace PandorasBox.Features.UI
         public override void Disable()
         {
             SaveConfig(Config);
-            Svc.Framework.Update -= FillRegularNumeric;
-            Svc.Framework.Update -= FillVentureNumeric;
-            //Svc.Framework.Update -= FillBankNumeric;
+            Common.OnAddonSetup -= FillRegularNumeric;
+            Common.OnAddonSetup -= FillVentureNumeric;
+            //Common.OnAddonSetup -= FillBankNumeric;
             base.Disable();
         }
 
@@ -317,8 +287,9 @@ namespace PandorasBox.Features.UI
                     {
                         minOrMax = -1;
                     }
+
+                    ImGui.Checkbox("Exclude Split Dialog", ref excludeSplit);
                 }
-                ImGui.Checkbox("Exclude Split Dialog", ref excludeSplit);
                 if (minOrMax != -1) ImGui.Checkbox("Auto Confirm", ref autoConfirm);
                 ImGui.Unindent();
                 ImGui.PopID();
