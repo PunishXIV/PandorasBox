1ee23144b65c25583ed584531bbd8f2f466b16ce
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2023-10-12 10:44:37 +0100

    Update CoordinatesHelper.cs

--- PandorasBox/Helpers/CoordinatesHelper.cs
+++ PandorasBox/Helpers/CoordinatesHelper.cs
@@ -11,32 +11,19 @@ namespace PandorasBox.Helpers
         public static Lumina.Excel.ExcelSheet<Aetheryte> Aetherytes = Svc.Data.GetExcelSheet<Aetheryte>(Svc.ClientState.ClientLanguage);
         public static Lumina.Excel.ExcelSheet<MapMarker> AetherytesMap = Svc.Data.GetExcelSheet<MapMarker>(Svc.ClientState.ClientLanguage);
 
-        public class MapLinkMessage
+        public class MapLinkMessage(ushort chatType, string sender, string text, float x, float y, float scale, uint territoryId, string placeName, DateTime recordTime)
         {
             public static MapLinkMessage Empty => new(0, string.Empty, string.Empty, 0, 0, 100, 0, string.Empty, DateTime.Now);
 
-            public ushort ChatType;
-            public string Sender;
-            public string Text;
-            public float X;
-            public float Y;
-            public float Scale;
-            public uint TerritoryId;
-            public string PlaceName;
-            public DateTime RecordTime;
-
-            public MapLinkMessage(ushort chatType, string sender, string text, float x, float y, float scale, uint territoryId, string placeName, DateTime recordTime)
-            {
-                ChatType = chatType;
-                Sender = sender;
-                Text = text;
-                X = x;
-                Y = y;
-                Scale = scale;
-                TerritoryId = territoryId;
-                PlaceName = placeName;
-                RecordTime = recordTime;
-            }
+            public ushort ChatType = chatType;
+            public string Sender = sender;
+            public string Text = text;
+            public float X = x;
+            public float Y = y;
+            public float Scale = scale;
+            public uint TerritoryId = territoryId;
+            public string PlaceName = placeName;
+            public DateTime RecordTime = recordTime;
         }
 
         public static string GetNearestAetheryte(MapLinkMessage maplinkMessage)
