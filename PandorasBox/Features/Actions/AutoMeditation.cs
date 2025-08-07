91ca001dc43c204a5f2a2a687612939d70870ffa
Kage <95875972+Kagekazu@users.noreply.github.com>
2025-08-07 16:14:38 +0200

    updated med for no spam

--- PandorasBox/Features/Actions/AutoMeditation.cs
+++ PandorasBox/Features/Actions/AutoMeditation.cs
@@ -2,60 +2,62 @@ using Dalamud.Game.ClientState.Conditions;
 using Dalamud.Game.ClientState.JobGauge.Types;
 using ECommons.DalamudServices;
 using ECommons.ExcelServices;
+using ECommons.EzHookManager;
 using ECommons.GameHelpers;
+using ECommons.Throttlers;
+using FFXIVClientStructs.FFXIV.Client.Game;
 using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using PandorasBox.FeaturesSetup;
+namespace PandorasBox.Features.Actions;
 
-namespace PandorasBox.Features.Actions
+internal class AutoMeditation : Feature
 {
-    internal class AutoMeditation : Feature
+    public override string Name => "Auto-Meditation";
+
+    public override string Description => "Automatically use Meditation when out of combat.";
+
+    public override FeatureType FeatureType => FeatureType.Actions;
+
+    public override void Enable()
     {
-        public override string Name => "Auto-Meditation";
+        Svc.Framework.Update += RunFeature;
+        Events.OnJobChanged += DelayStart;
+        if (SendActionHook is null) EzSignatureHelper.Initialize(this);
+        else SendActionHook?.Enable();
+        base.Enable();
+    }
 
-        public override string Description => "Automatically use Meditation when out of combat.";
+    private static void DelayStart(uint? jobId)
+    {
+        EzThrottler.Throttle("MNKMed", 3000);
+    }
 
-        public override FeatureType FeatureType => FeatureType.Actions;
+    private static unsafe void RunFeature(IFramework framework)
+    {
+        if (Player.Object is null) return;
+        var isMonk = Player.Job == Job.MNK;
+        var isPugilist = Player.Job == Job.PGL;
+        if (!isMonk && !isPugilist) return;
+        var gauge = Svc.Gauges.Get<MNKGauge>();
+        if (gauge.Chakra == 5) return;
+        if (Svc.Condition[ConditionFlag.InCombat]) return;
+        if (TerritoryInfo.Instance()->InSanctuary) return;
 
-        public override void Enable()
+        if (!Svc.Condition[ConditionFlag.InCombat] && EzThrottler.Throttle("PCTMotifs", 1500))
         {
-            Svc.Framework.Update += RunFeature;
-            base.Enable();
+            var am = ActionManager.Instance();
+            if (am->GetActionStatus(ActionType.Action, 36942) == 0)
+                am->UseAction(ActionType.Action, 36942);
+            if (am->GetActionStatus(ActionType.Action, 36940) == 0)
+                am->UseAction(ActionType.Action, 36940);
         }
+    }
 
-        private unsafe void RunFeature(IFramework framework)
-        {
-            if (Player.Object is null) return;
-            var isMonk = Player.Job == Job.MNK;
-            var isPugilist = Player.Job == Job.PGL;
-            if (!isMonk && !isPugilist) return;
-            var gauge = Svc.Gauges.Get<MNKGauge>();
-            if (gauge.Chakra == 5) return;
-
-            if (!Svc.Condition[ConditionFlag.InCombat])
-            {
-                TaskManager.EnqueueDelay(1000);
-                TaskManager.Enqueue(() =>
-                {
-                    if (Svc.Condition[ConditionFlag.InCombat]) return;
-
-                    if (TerritoryInfo.Instance()->InSanctuary) return;
-                    if (Player.Level >= 54 && isMonk && IsActionUnlocked(36942))
-
-                    {
-                        UseAction(36942);
-                    }
-                    else
-                    {
-                        UseAction(36940);
-                    }
-                });
-            }
-        }
-
-        public override void Disable()
-        {
-            Svc.Framework.Update -= RunFeature;
-            base.Disable();
-        }
+    public override void Disable()
+    {
+        Svc.Framework.Update -= RunFeature;
+        Events.OnJobChanged -= DelayStart;
+        SendActionHook?.Disable();
+        base.Disable();
     }
 }
