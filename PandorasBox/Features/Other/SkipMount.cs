84d6c7ec9204c141eea0952e45244feeef74e0de
Taurenkey <github@taurenkey.co.uk>
2026-02-23 18:39:42 +0000

    Skip mount in macros

---
+++ PandorasBox/Features/Other/SkipMount.cs
@@ -0,0 +1,46 @@
+using ECommons.DalamudServices;
+using ECommons.EzHookManager;
+using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.UI.Shell;
+using PandorasBox.FeaturesSetup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PandorasBox.Features.Other
+{
+    internal class SkipMount : Feature
+    {
+        public override string Name => "Skip '/mount' In Macros If Mounted";
+
+        public override string Description => "Using a macro with /mount in it will skip that line if you're already mounted";
+
+        public override FeatureType FeatureType => FeatureType.Other;
+
+        public override void Enable()
+        {
+            if (UseActionHook is null) EzSignatureHelper.Initialize(this);
+            else UseActionHook?.Enable();
+            base.Enable();
+        }
+
+        public override unsafe bool UseActionDetour(ActionManager* actionManager, ActionType actionType, uint actionId, ulong targetId, uint extraParam, ActionManager.UseActionMode mode, uint comboRouteId, bool* outOptAreaTargeted)
+        {
+            var macroLine = RaptureShellModule.Instance()->MacroCurrentLine;
+            if (macroLine > 0)
+            {
+                var line = RaptureShellModule.Instance()->MacroLineText;
+                if (line.ToString().StartsWith("/mount") && Svc.Condition[Dalamud.Game.ClientState.Conditions.ConditionFlag.Mounted])
+                    return false;
+            }
+            return base.UseActionDetour(actionManager, actionType, actionId, targetId, extraParam, mode, comboRouteId, outOptAreaTargeted);
+        }
+
+        public override void Disable()
+        {
+            UseActionHook?.Disable();
+            base.Disable();
+        }
+    }
+}
